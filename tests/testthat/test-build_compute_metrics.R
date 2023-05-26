a5fe254efbd2a41677fbf1e4ecace4f28a93fb1e
Marc Ohlmann <marcohlmann@live.fr>
2023-05-26 17:38:54 +0200

    update test 2

--- tests/testthat/test-build_compute_metrics.R
+++ tests/testthat/test-build_compute_metrics.R
@@ -19,11 +19,11 @@ test_that("TL computation",  {
 })
 
 meta0 = compute_TL(meta0)
+metrics_angola = compute_metrics(meta_angola)
 
 test_that("dimension of metrics table",{
   expect_equal(length(compute_metrics(meta0)),4)
   expect_equal(class(compute_metrics(meta0)),"numeric")
-  metrics_angola = compute_metrics(meta_angola)
   expect_equal(length(metrics_angola),2)
   expect_equal(dim(metrics_angola$Species),c(3,4))
 })
